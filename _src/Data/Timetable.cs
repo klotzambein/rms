aa00b12c94c6bd3bb79387ab99e6ee359c8d5812
Robin Kock <robin@kock-hamburg.de>
2017-08-31 17:04:51 +0200

    removed department in Timetable

--- _src/Data/Timetable.cs
+++ _src/Data/Timetable.cs
@@ -8,22 +8,19 @@ namespace WebUnitsApiRipper.Data
 {
     public class Timetable
     {
-        public Timetable(DateTime timeStamp, Class activeClass, IntervalTree<DateTime, Lesson> lessons, Dictionary<int, Department> departments, Dictionary<long, LessonInfo> infos)
+        public Timetable(DateTime timeStamp, Class activeClass, IntervalTree<DateTime, Lesson> lessons, Dictionary<long, LessonInfo> infos)
         {
             TimeStamp = timeStamp;
             ActiveClass = activeClass;
             Lessons = lessons;
-            Departments = departments;
             Infos = infos;
         }
 
-        public Timetable(Class activeClass, List<Department> departments, JsonClassesStage2.Result result)
+        public Timetable(Class activeClass, JsonClassesStage2.Result result)
         {
             TimeStamp = DateTimeOffset.FromUnixTimeMilliseconds(result.lastImportTimestamp).DateTime;
 
             ActiveClass = activeClass;
-
-            Departments = departments.ToDictNoDups(d => d.Id);
 
             Infos = result.data.elements
                 .Select(e => new LessonInfo(e))
@@ -45,7 +42,8 @@ namespace WebUnitsApiRipper.Data
         public DateTime TimeStamp { get; }
         public Class ActiveClass { get; }
         public IntervalTree<DateTime, Lesson> Lessons { get; }
-        public Dictionary<int, Department> Departments { get; }
         public Dictionary<long, LessonInfo> Infos { get; }
+
+
     }
 }
