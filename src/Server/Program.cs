b4d91d635bad2fd4d373d68e8b57bce6832159bd
Robin Kock <robin@kock-hamburg.de>
2017-09-29 16:43:08 +0200

    Program starting to implement joining

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 //using System.Globalization;
 using System.IO;
@@ -7,20 +8,28 @@ using System.Reflection;
 using GoogleCalendarApi;
 using Ical.Net;
 using Ical.Net.DataTypes;
+using Ical.Net.Interfaces.DataTypes;
 using ICal;
 using Newtonsoft.Json;
 using NLog;
+using WebUntis;
+using WebUntis.Data;
 
 namespace Server
 {
     class Program
     {
+        static Dictionary<string, WebUntisApi> webUntisDict = new Dictionary<string, WebUntisApi>();
         static void Main(string[] args)
         {
 #if DEBUG
             Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
 #endif
             var config = RMSConfig.LoadFile("Config.json");
+
+            var scheduler = new Scheduler();
+            foreach (var dt in config.UpdateCycleDT) scheduler.Add(dt, OnUpdateCycle);
+
             /*var now = DateTime.Now;
 
             Calendar calendar = new Calendar();
@@ -45,6 +54,53 @@ namespace Server
                 .UseSpecificPrefixes("http://localhost:\\p/")
                 .UseCache(cache);
             api.Start();*/
+        }
+
+        static WebUntisApi GetWebUntisApi(string sourceSchool, string sourceDomain)
+        {
+            string source = $"{sourceSchool}@{sourceDomain.ToLower()}";
+
+            WebUntisApi api;
+
+            if (webUntisDict.TryGetValue(source, out api))
+                return api;
+
+            api = new WebUntisApi(sourceDomain, sourceSchool);
+            webUntisDict.Add(source, api);
+            return api;
+        }
+
+        static void AddTimetableToCalendar(TimeTable timeTable, Calendar calendar, IEnumerable<CourseConfig> courses)
+        {
+            var now = DateTime.Now;
+            var newCal = new Calendar();
+
+            foreach (var e in calendar.Events
+                .Where(e => e.End.AsSystemLocal < now))
+                newCal.Events.Add(e);
+
+            foreach (var ls in timeTable.Lessons)
+            {
+                if (ls.Key.End < now)
+                    continue;
+
+                foreach (var l in ls.Value)
+                {
+                    
+                    CalendarEvent item = new CalendarEvent()
+                    {
+                        DtStart = new CalDateTime(ls.Key.Start),
+                        DtEnd = new CalDateTime(ls.Key.End),
+                        Summary = l.ToString(),
+                    };
+                    newCal.Events.Add(item);
+                }
+            }
+        }
+
+        static void OnUpdateCycle()
+        {
+
         }
     }
 }
